bc0da1658d3282190010608dea7a6bcf387af119
Kamila Carvalho <kamilaingridc@gmail.com>
2025-03-27 07:39:56 -0300

    ?

--- src/main/java/br/com/ingrid/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/ingrid/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,16 @@
 package br.com.ingrid.screenmatch;
 
+import br.com.ingrid.screenmatch.model.DadosEpisodio;
 import br.com.ingrid.screenmatch.model.DadosSerie;
+import br.com.ingrid.screenmatch.model.DadosTemporada;
 import br.com.ingrid.screenmatch.service.ConsumoAPI;
 import br.com.ingrid.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -24,5 +29,18 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+
+		json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=3461c496");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i<=dados.totalTemporadas(); i++) {
+			json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=3461c496");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
 	}
 }
