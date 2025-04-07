05930445ebf5bb7a7e6b3a45e45ba133f4480db6
Kamila Carvalho <kamilaingridc@gmail.com>
2025-04-07 09:13:44 -0300

    Explicação do .steam() em java

--- src/main/java/br/com/ingrid/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/ingrid/screenmatch/ScreenmatchApplication.java
@@ -1,16 +1,9 @@
 package br.com.ingrid.screenmatch;
 
-import br.com.ingrid.screenmatch.model.DadosEpisodio;
-import br.com.ingrid.screenmatch.model.DadosSerie;
-import br.com.ingrid.screenmatch.model.DadosTemporada;
-import br.com.ingrid.screenmatch.service.ConsumoAPI;
-import br.com.ingrid.screenmatch.service.ConverteDados;
+import br.com.ingrid.screenmatch.main.Main;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -21,26 +14,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoAPI = new ConsumoAPI();  // consumindo ConsumoAPI
-		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=3461c496");
-		System.out.println(json);
 
-		ConverteDados conversor = new ConverteDados();
+		Main main = new Main();
+		main.exibeMenu();
 
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-
-		json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=3461c496");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for (int i = 1; i<=dados.totalTemporadas(); i++) {
-			json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=3461c496");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-		temporadas.forEach(System.out::println);
 	}
 }
---
+++ src/main/java/br/com/ingrid/screenmatch/main/Main.java
@@ -0,0 +1,66 @@
+package br.com.ingrid.screenmatch.main;
+
+import br.com.ingrid.screenmatch.model.DadosEpisodio;
+import br.com.ingrid.screenmatch.model.DadosSerie;
+import br.com.ingrid.screenmatch.model.DadosTemporada;
+import br.com.ingrid.screenmatch.service.ConsumoAPI;
+import br.com.ingrid.screenmatch.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private ConverteDados conversor = new ConverteDados();
+
+    private final String ENDERECO="https://www.omdbapi.com/?t=";
+    private final String API_KEY="&apikey=3461c496";
+
+    public void exibeMenu() {
+        System.out.println("Digite o nome da série para busca: ");
+        var nomeSerie = leitura.nextLine();
+
+        var json = consumoAPI.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i<=dados.totalTemporadas(); i++) {
+			json = consumoAPI.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()) // puxar todas listas juntas (episodios)
+                // .collect(Collectors.toList());  // joga em uma lista
+                .toList();  // resulta uma lista imutável (não consegue alterar)
+
+
+//        for (int i = 0; i < dados.totalTemporadas(); i++) {
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for (int j = 0; j < episodiosTemporada.size(); j++) {
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
+
+
+//        List<String> nomes = Arrays.asList("Marcela", "Laura", "Vanessa", "Ingrid", "Teste");
+//
+//        nomes.stream()  // pode-se criar tudo em uma linha só
+//                .sorted()  // ordena alfabeticamente
+//                .limit(3) // vai pegar a lista de nomes com os três primeiros
+//                .filter(n -> n.startsWith("M")) // filtra os nomes começa com M
+//                .map(n -> n.toUpperCase()) // coloca como letra maiúscula
+//                .forEachOrdered(System.out::println);  // printa depois de todas exigencias
+    }
+}
