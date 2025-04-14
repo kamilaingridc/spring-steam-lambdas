82d21c72c905a4b6e3361b2f19b5b10bb30d199e
Kamila Carvalho <kamilaingridc@gmail.com>
2025-04-13 23:52:10 -0300

    Formatando datas e filtrando por ano

--- src/main/java/br/com/ingrid/screenmatch/main/Main.java
+++ src/main/java/br/com/ingrid/screenmatch/main/Main.java
@@ -7,6 +7,8 @@ import br.com.ingrid.screenmatch.model.Episodio;
 import br.com.ingrid.screenmatch.service.ConsumoAPI;
 import br.com.ingrid.screenmatch.service.ConverteDados;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -56,6 +58,21 @@ public class Main {
                 ).collect(Collectors.toList());
 
         episodios.forEach(System.out::println);
+
+        System.out.println("A partir de que ano deseja ver os episódios?");
+        var ano = leitura.nextInt();
+        leitura.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        episodios.stream()
+                .filter( e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                                "Episódio: " + e.getTitulo() +
+                                "Data Lançamento: " + e.getDataLancamento().format(formatador)
+                ));
 
 //        for (int i = 0; i < dados.totalTemporadas(); i++) {
 //            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
