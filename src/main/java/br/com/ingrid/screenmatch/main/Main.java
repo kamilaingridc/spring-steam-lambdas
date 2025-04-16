8f22614fe0cfeeab996a7fba38fbe78e87f0e921
Kamila Carvalho <kamilaingridc@gmail.com>
2025-04-16 09:08:58 -0300

    Coletando estatísticas com DoubleSummaryStatistics

--- src/main/java/br/com/ingrid/screenmatch/main/Main.java
+++ src/main/java/br/com/ingrid/screenmatch/main/Main.java
@@ -121,5 +121,16 @@ public class Main {
                                         Episodio::getAvaliacao // Função que extrai o valor da avaliação do episódio (double)
                                 )
                         ));
+        System.out.println(avaliacoesPorTemporada);
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episódio: " + est.getMax());
+        System.out.println("Pior episódio: " + est.getMin());
+        System.out.println("Quantidade: " + est.getCount());
+
+        // Estatísticas, relatórios, streams e lambdas fazem muita parte do dia a dia! :)
     }
 }
